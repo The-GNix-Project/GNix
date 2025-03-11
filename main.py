087662fefd31e56e5150767e388b961cee7f8250
= <=>
2025-03-11 19:53:46 +1000

    changed to python kivy

---
+++ main.py
@@ -0,0 +1,4 @@
+from src import GNix
+
+if __name__ == "__main__":
+    GNix().run()
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+import os
+# IMPORTANT, SET ENVIRONMENT VARIABLES HERE
+# os.environ['KIVY_TEXT'] = 'pil' 
+
+import kivy
+kivy.require('2.1.0')
+
+from .app import GNix

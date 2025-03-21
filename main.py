fea2fcd5a7c6b3bd1d5e7b3650f2e81d1379f0e1
= <=>
2025-03-21 23:06:51 +1000

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from nix_parser import parse_nix, find_key_pair
 
-parsed = parse_nix("{imports = {1, 2, 3}}")
-
-print(find_key_pair(parsed, "imports"))
+parsed = parse_nix("{imports = {a=1; b=2; c=3;};}")
+print(find_key_pair(parsed, "imports").get("Map").get("bindings"))

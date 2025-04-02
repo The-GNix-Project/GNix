41cca65ddc89fe551dfc46ad41a4f9a201aa6859
Archie Hickmott <100406766+ArchieHickmott@users.noreply.github.com>
2025-04-02 12:03:48 +1000

    added license headers to all code, GNix is FREE software!!!!

--- nix_parser/src/utils.rs
+++ nix_parser/src/utils.rs
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This file is part of GNix.
+//
+// GNix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+//
+// GNix is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GNix.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::parser::grammar::*;
 
 use pyo3::prelude::*;
